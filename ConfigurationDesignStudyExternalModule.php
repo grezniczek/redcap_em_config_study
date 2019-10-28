ca76e3306fca9f035009ecfba9f9507e40ca5bc4
G. Rezniczek <g.rezniczek@gmail.com>
2019-10-28 18:52:40 +0100

    Implement module

---
+++ ConfigurationDesignStudyExternalModule.php
@@ -0,0 +1,18 @@
+<?php
+namespace DE\RUB\ConfigurationDesignStudyExternalModule;
+
+use ExternalModules\AbstractExternalModule;
+
+/**
+ * ExternalModule class for Configuration Design Study.
+ */
+class ConfigurationDesignStudyExternalModule extends AbstractExternalModule {
+
+
+    function __construct() {
+        parent::__construct();
+    }
+
+
+    
+}
